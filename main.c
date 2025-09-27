8871ea778d8d298b5b0e465d86791248ac6a1c21
DinaTolotra <ratsinjomananatolotra@gmail.com>
2025-09-27 13:45:51 +0300

    using libfs in main.c

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include "./include/libio.h"
-#include <fcntl.h>
+#include "./include/libfs.h"
 
 char *parse_arg(int ac, char **av);
-int open_file(char *file);
 
 int main(int ac, char **av)
 {
@@ -11,7 +10,7 @@ int main(int ac, char **av)
   char *file;
 	
   file = parse_arg(ac, av);
-	fd = open_file(file);
+  fd = fs_openfile_for_read(file);
   if (fd >= 0)
   {
     while (fd >= 0 && io_getchar(fd, &c) > 0)
@@ -23,16 +22,6 @@ int main(int ac, char **av)
     io_putstr(1, "Failed to open file\n");
   }
 	return (0);
-}
-
-int open_file(char *file)
-{
-  int fd;
-
-  fd = open(file, O_RDONLY);
-  if (fd < 0)
-    fd = -1;
-  return (fd);
 }
 
 char *parse_arg(int ac, char **av)
